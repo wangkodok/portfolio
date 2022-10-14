aaa5eb63c257f224cd25497135a791a5471b5e0f
wangkodok <wangkodok@gmail.com>
2022-10-14 23:41:42 +0900

    햄버거 메뉴 애니메이션 추가

--- js/common.js
+++ js/common.js
@@ -3,23 +3,44 @@ const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const menu = document.querySelector('.menu');
 const menuOpen = document.querySelector('.full-menu-content');
-let scope = 0;
-menu.addEventListener('click', menuBtn);
-function menuBtn() {
-  if (scope === 0) {
+// let scope = 0;
+// menu.addEventListener('click', menuBtn);
+// function menuBtn() {
+//   if (scope === 0) {
+//     header.style.position = 'fixed';
+//     header.style.zIndex = 1001;
+//     menuOpen.classList.add('on');
+//     menu.classList.add('on');
+//     menu.style.zIndex = 1001;
+//     scope = 1;
+//   } else {
+//     header.style.position = '';
+//     header.style.zIndex = '';
+//     menuOpen.classList.remove('on');
+//     menu.classList.remove('on');
+//     menu.style.zIndex = '';
+//     window.scrollTo(0, 0);
+//     scope = 0;
+//   }
+// }
+
+// 햄버거 메뉴
+const btnMenu = document.querySelector('.btn-line');
+console.log(btnMenu);
+btnMenu.addEventListener('click', () => {
+  if (!btnMenu.classList.contains('on')) {
     header.style.position = 'fixed';
     header.style.zIndex = 1001;
     menuOpen.classList.add('on');
-    menu.classList.add('on');
-    menu.style.zIndex = 1001;
-    scope = 1;
+    btnMenu.classList.add('on');
+    btnMenu.classList.remove('off');
+    btnMenu.style.zIndex = 1001;
   } else {
     header.style.position = '';
     header.style.zIndex = '';
     menuOpen.classList.remove('on');
-    menu.classList.remove('on');
-    menu.style.zIndex = '';
-    window.scrollTo(0, 0);
-    scope = 0;
+    btnMenu.classList.remove('on');
+    btnMenu.classList.add('off');
+    btnMenu.style.zIndex = 1001;
   }
-}
+})
