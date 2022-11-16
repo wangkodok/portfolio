bea8bf878dcccc2947f549efa2b3b0a2802bd2b0
wangkodok <wangkodok@gmail.com>
2022-11-16 20:10:13 +0900

    스크롤 애니메이션 코드 추가

--- js/common.js
+++ js/common.js
@@ -44,3 +44,25 @@ btnMenu.addEventListener('click', () => {
     btnMenu.style.zIndex = 1001;
   }
 })
+
+// 스크롤 애니메이션
+window.addEventListener('load', () => {
+
+  function scrollAppear() {
+    const textBox = document.querySelectorAll('.text-content p');
+    const screenPosition = window.innerHeight / 1;
+
+    for (let i = 0; i < textBox.length; i++) {
+      const textPosition = textBox[i].getBoundingClientRect().top;
+
+      if (textPosition < screenPosition) {
+        textBox[i].classList.add('scroll-on');
+      } else {
+        textBox[i].classList.remove('scroll-on');
+      }
+    }
+  }
+
+  window.addEventListener('scroll', scrollAppear);
+
+});
