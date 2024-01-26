1794bfeb248ed6e0e2abebd7cbc96aa3434e5e86
wangkodok <wangkodok@gmail.com>
2024-01-26 20:28:38 +0900

    Repositories 페이지에서 레이아웃 작업

--- src/components/RepositoriesInfo/RepositoriesInfo.jsx
+++ src/components/RepositoriesInfo/RepositoriesInfo.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
 import style from "./RepositoriesInfo.module.css";
 
 export default function RepositoriesInfo() {
   return (
     <section className={style["repositories-content"]}>
+      <h2 className={style.title}>콘텐츠 제목</h2>
       <ul className={style.list}>
         <li className={style.item}>
-          <div>콘텐츠 추가 예정</div>
+          <div>
+            <Link to="">콘텐츠 추가 예정</Link>
+            <p>콘텐츠 설명을 작성하십시오.</p>
+          </div>
         </li>
       </ul>
     </section>
